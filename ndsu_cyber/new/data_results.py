1c7ae632db2fee11194495e1d36311e02460eb4e
Collin Gros <vexprof@gmail.com>
2018-09-23 14:58:05 -0500

    test 15

--- ndsu_cyber/new/data_results.py
+++ ndsu_cyber/new/data_results.py
@@ -16,7 +16,8 @@ class Test:
                      "accuracy": -1.0,
                      "total_imgs": 0,
                      "reviewed_imgs": 0,
-                     "skipped_imgs": 0
+                     "skipped_imgs": 0,
+                     "id": -1
                      }
 
 
@@ -33,20 +34,15 @@ def print_tests(tests):
 
 
 def show_results(tests):
+    '''
     ordered_acc = []
     last_acc = -1
     for test in tests:
         acc = test.data["accuracy"]
-        print("acc: " + str(acc))
-        print("last_acc: " + str(last_acc))
         if last_acc == -1 and acc <= 100:
-            print("last_acc is -1 and acc <= 100")
             last_acc = acc
         elif last_acc != -1 and acc > last_acc and acc <= 100:
-            print("adding test!")
             ordered_acc.append(test)
-        else:
-            print("failed all if")
 
     ordered_acc = ordered_acc[-10:]
 
@@ -58,13 +54,35 @@ def show_results(tests):
         for key, value in test.data.items():
             print(key + ":\t" + str(value))
         input()
-            
+    '''
+
+    acc = []
+    while test_c < 10:
+        for test in tests:
+            for key, value in test.items():
+                if key == "reviewed_imgs":
+                    reviewed = value
+                elif key == "skipped_imgs":
+                    skipped = value
+
+            if (reviewed <= 200 and reviewed >= last_r) and skipped <= 
+            # if the num of detected faces is about the same as the num
+            # of images and the current value is better than the last
+            # set new max
+                new_max = test
+        test_c += 1
 
 
 def add_data(tests, path):
     new_test = Test()
 
     new_test.data["path"] = path
+
+    path_subs = path.split("/")
+    test_id = ''.join(filter(str.isdigit, path_subs[-1])
+    new_test.data["id"] = test_id
+    # need a unique test id for differentiating tests, test num works
+    # (test num is int at last part of path)
 
     for item in os.listdir(path):
         item_path = path + "/" + item
@@ -79,45 +97,29 @@ def add_data(tests, path):
                 for line in info:
                     line_subs = line.strip("\n").split("\t")
 
-                    # COMMENTING UNTIL TESTED DATA IS SORTED CORRECTLY!!!
-                    
                     if line_c == 1:
                         speed = line_subs[-1].split(" ")[0]
                         new_test.data["speed"] = float(speed)
-                        #print("adding speed...")
-                        #print(str(speed))
                     elif line_c == 5:
                         new_test.data["cascade"] = line_subs[-1]
-                        #print("adding cascade...")
-                        #print(str(line_subs[-1]))
                     elif line_c == 19:
                         new_test.data["sf"] = float(line_subs[-1])
-                        #print("adding sf...")
-                        #print(str(line_subs[-1]))
                     elif line_c == 15:
                         new_test.data["mn"] = int(line_subs[-1])
-                        #print("adding mn...")
-                        #print(str(line_subs[-1]))
                     elif line_c == 23:
                         new_test.data["res"] = int(line_subs[-1])
-                        #print("adding res...")
-                        #print(str(line_subs[-1]))
                     elif line_c == 33:
                         if line_subs[-1] == "skipped_is_0":
-                            new_test.data["accuracy"] = 0
+                            new_test.data["accuracy"] = -1
                         else:
                             new_test.data["accuracy"] = float(line_subs[-1])
-                        #print("adding accuracy...")
-                        #print(str(line_subs[-1]))
                     elif line_c == 29:
                         new_test.data["reviewed_imgs"] = int(line_subs[-1])
                     elif line_c == 30:
                         new_test.data["skipped_imgs"] = int(line_subs[-1])
                     elif line_c == 31:
                         new_test.data["total_imgs"] = int(line_subs[-1])
-                    
 
-                    #print("item_path: " + item_path)
                     line_c += 1
 
         elif item.split(".")[1] == "JPG":
@@ -127,14 +129,13 @@ def add_data(tests, path):
         elif item.split("/")[-1] == "labels.pickle":
             new_test.data["labels"] = item_path
 
-    if (new_test.data["reviewed_imgs"] < new_test.data["total_imgs"] and
-        new_test.data["accuracy"] == 0):
+    if new_test.data["accuracy"] == -1:
         # "accuracy" only means # of faces detected / # of imgs skipped
         # so if skipped is 0, and there are an unreasonable amount of
         # faces detected, the accuracy is bad, this if is to correct the
         # assumtion of a bad result by checking if # of faces detected
         # is a reasonable number (300 is the total # of tests)
-        new_test.accuracy = 100
+        new_test.data["accuracy"] = new_test.data["reviewed_imgs"]
 
     tests.append(new_test)
 
@@ -145,7 +146,6 @@ for result_dir in sorted(os.listdir(parent_dir + "out/")):
 
     add_data(tests, result_path)
 
-#print_tests(tests)
-print("now show results")
-show_results(tests)
+print_tests(tests)
+#show_results(tests)
 
