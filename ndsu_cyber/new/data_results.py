2476ed285289155a059596cab7fc2dd3976f0da2
Collin Gros <vexprof@gmail.com>
2018-09-23 19:35:13 -0500

    test 26

--- ndsu_cyber/new/data_results.py
+++ ndsu_cyber/new/data_results.py
@@ -71,6 +71,9 @@ def show_pics(tests):
 
                 img_c = 1
                 for img in sorted(test.data["imgs"]):
+                    img_name = ("TEST #" + str(test_c) + "/" +
+                                str(len(tests)) + "\tIMG #" + str(img_c)) +
+                                "/" + str(len(tests.data["imgs"])))
                     cv2_img = cv2.imread(img, 0)
                     cv2.resize(cv2_img, (150, 100))
                     cv2.imshow(img_name, cv2_img)
@@ -92,6 +95,9 @@ def show_pics(tests):
 
                 img_c = 1
                 for img in sorted(test.data["imgs"]):
+                    img_name = ("TEST #" + str(test_c) + "/" +
+                                str(len(tests)) + "\tIMG #" + str(img_c)) +
+                                "/" + str(len(tests.data["imgs"])))
                     cv2_img = cv2.imread(img, 0)
                     cv2.resize(cv2_img, (150, 100))
                     cv2.imshow(img_name, cv2_img)
