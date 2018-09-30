04ca30ac1977349fb741669e601e6ffeccac401f
Collin Gros <vexprof@gmail.com>
2018-09-30 18:32:57 -0500

    debug

--- ndsu_cyber/new/test_data.sh
+++ ndsu_cyber/new/test_data.sh
@@ -14,15 +14,15 @@ do
             for res in 150
             do
                 ((dir_n++))
-                rm -r test/opt_$dir_n
-                mkdir test/opt_$dir_n
+                rm -r out2/opt_$dir_n
+                mkdir out2/opt_$dir_n
 
-                cp out/opt_$dir_n/train.yml test/opt_$dir_n/train.yml
-                cp out/opt_$dir_n/labels.pickle test/opt_$dir_n/labels.pickle
+                cp out/opt_$dir_n/train.yml out2/opt_$dir_n/train.yml
+                cp out/opt_$dir_n/labels.pickle out2/opt_$dir_n/labels.pickle
 
                 python3 test.py -s $sf -n $mn -p $res -l 1 -v 1 -o 1 -i 1 \
-                -z $casc -g 1 -a 1 -k test/opt_$dir_n \
-                > ./test/opt_$dir_n/scriptstat.txt
+                -z $casc -g 1 -a 1 -k out2/opt_$dir_n \
+                > ./out2/opt_$dir_n/scriptstat.txt
             done
         done
     done
