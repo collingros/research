711704b6aa40a5d4f7da7342dc9bf75d641f1136
Collin Gros <vexprof@gmail.com>
2018-10-25 09:34:38 -0500

    debug

--- ndsu_cyber/new/data.py
+++ ndsu_cyber/new/data.py
@@ -148,13 +148,10 @@ class Test:
         for result in results_filter:
             print("result: {0}".format(result))
             for person, arrs in eval(self.data["results"][result]).items():
-                # FIXME: i assumed that results[result] was a dict
-                #        but is in fact just a printed dict, so a str...
-                print("person: {0}\narrs: {1}".format(person, arrs))
                 if result == "c_names":
-                    num_c += 1 + arrs[1][0]
+                    num_c += 1 + arrs[1]
                 else:
-                    num_w += 1 + arrs[1][0]
+                    num_w += 1 + arrs[1]
 
 
         print("test id:\t{0}\n"
