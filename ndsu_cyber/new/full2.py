4f58ab91bb74fdfe3e496d785ba69adb55e6f08a
Collin Gros <vexprof@gmail.com>
2018-10-30 18:08:09 -0500

    debug

--- ndsu_cyber/new/full2.py
+++ ndsu_cyber/new/full2.py
@@ -28,7 +28,7 @@ def reverse_all(path, pos, angle):
 
     full_path = "{0}{1}/{2}/".format(path, pos, angle)
     for img in sorted(os.listdir(full_path)):
-        src = "{0}{1}/".format(full_path, img)
+        src = "{0}{1}".format(full_path, img)
 
         if pos == "pos_0":
             dst = "{0}pos_4/".format(path)
@@ -56,7 +56,7 @@ def reverse_all(path, pos, angle):
         elif angle == "angle_7":
             dst = "{0}angle_1/{1}".format(dst, img)
 
-        print("flopping\nsrc: {0}\ndst:{1}".format(src, dst))
+        print("flopping\nsrc: {0}\ndst: {1}".format(src, dst))
         flop(src, dst)
 
 
@@ -70,13 +70,15 @@ for person in sorted(os.listdir(cwd)):
     for base in sorted(os.listdir(person_path)):
         base_path = "{0}{1}/".format(person_path, base)
 
-        for pos in sorted(os.listdir(base_path)):
+        for pos in sorted(os.listdir(base_path), reverse=True):
             pos_path = "{0}{1}/".format(base_path, pos)
 
             if pos == "pos_2":
                 continue
             elif pos == "pos_3" or pos == "pos_4":
+                print("removing {0}".format(pos_path))
                 rm_dir(pos_path)
+                print("creating {0}".format(pos_path))
                 mk_dir(pos_path)
 
             for angle in sorted(os.listdir(pos_path)):
