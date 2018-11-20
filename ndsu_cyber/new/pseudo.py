2fc0aa26ccdd48f64db43c78766ceb3f248d82bc
Collin Gros <vexprof@gmail.com>
2018-11-20 15:08:06 -0600

    work

--- ndsu_cyber/new/pseudo.py
+++ ndsu_cyber/new/pseudo.py
@@ -46,13 +46,107 @@ class Settings:
         }
         self.trained = []
 
+        self.init_states()
+
+
+        def init_states(self):
+        # set dicts to initial testing/training values
+            pass
+
+        def rotate(self, type):
+        # 'rotate' a value in testing/training dicts to next value
+        # to be tested/trained
+            pass
+
+
+        def build_cmd(self):
+            cmd = ""
+            if self.is_test:
+                cmd += "python3 test.py "
+            else:
+                cmd += "python3 train.py "
+
+            for key, value in self.prog.items():
+                if key == "sf":
+                    cmd += "-s " + str(value)
+                elif key == "mn":
+                    cmd += "-n " + str(value)
+                elif key == "res":
+                    cmd += "-p " + str(value)
+                elif key == "c":
+                    cmd += "-z " + str(value)
+
+            for key, value in self.color.items():
+                if key == "w":
+                    cmd += "-w " + str(value)
+                elif key == "c":
+                    cmd += "-c " + str(value)
+                elif key == "l":
+                    cmd += "-l " + str(value)
+                elif key == "m":
+                    cmd += "-m " + str(value)
+                elif key == "h":
+                    cmd += "-b " + str(value)
+
+            for key, value in self.occ.items():
+                if key == "v":
+                    cmd += "-v " + str(value)
+                elif key == "h":
+                    cmd += "-f " + str(value)
+                elif key == "g":
+                    cmd += "-e " + str(value)
+
+            for key, value in self.pos.items():
+                if key == "p":
+                    cmd += "-x " + str(value)
+                elif key == "a":
+                    cmd += "-a " + str(value)
+                elif key == "c":
+                    cmd += "-o " + str(value)
+
+            for key, value in self.light.items():
+                if key == "s":
+                    cmd += "-g " + str(value)
+                elif key == "c":
+                    cmd += "-i " + str(value)
+
+            return cmd
+
+
+        def pull_train(self):
+        # pull trained data from stockpile, copy it into testing dir
+            pass
+
+
+        def save_train(self):
+        # copy fresh trained data into testing directory
+            pass
+
 
         def train(self):
-            pass
+            cmd = self.build_cmd()
+            # python3 train.py -blah blah blah
+
+            if self.already_trained():
+            # if we already trained using these settings
+                self.pull_train()
+                # pull the trained data from our stockpile of trained data
+                # and copy it into the testing directory
+            else:
+            # otherwise, if we didnt train using these settings
+                run_cmd(cmd)
+                # train using these settings
+                self.save_train()
+                # and copy it into the testing directory
 
 
         def test(self):
-            pass
+        # ** only works AFTER training data is present (obviously)
+            cmd = self.build_cmd()
+            # python3 test.py -blah blah blah
+
+            # using the training data in our testing directory,
+            # run the test
 
 
         def already_trained(self):
@@ -86,21 +180,27 @@ class Settings:
         pass
 
 
+def mkdir(name):
+# (shell) mkdir cmd
+    cmd = "mkdir {0}".format(name)
+    run_cmd(cmd)
+
+
+def mv(src, dst):
+# (shell) mv cmd
+    cmd = "mv {0} {1}".format(src, dst)
+    run_cmd(cmd)
+
+
 def run_cmd(cmd):
+# run a shell cmd
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     process.wait()
 
 
-def train(settings):
-    for key, value in settings.prog.items():
-        if key == "sf":
-            
+def init_dirs():
+# create testing dir, trained data stockpile dir, results/graphs dir
     pass
-
-
-def test(settings):
-    pass
-
 
 train_settings = Settings(False)
 test_settings = Settings(True)
