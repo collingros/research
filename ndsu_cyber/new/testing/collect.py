c01ec58e274c7753e134efa0a4d068afdd499878
Collin Gros <vexprof@gmail.com>
2018-12-27 18:46:33 -0700

    organization

---
+++ ndsu_cyber/new/testing/collect.py
@@ -0,0 +1,43 @@
+import os
+import subprocess
+
+
+def reset():
+    cmd = "./reset.sh"
+    run_cmd(cmd)
+
+
+def data(test_type):
+    cmd = "python3 ./progs/data.py -i {0}".format(test_type)
+    run_cmd(cmd)
+
+
+def cp(src, dst):
+    cmd = "cp -r {0} {1}".format(src, dst)
+    run_cmd(cmd)
+
+
+def mv(src, dst):
+    cmd = "mv {0} {1}".format(src, dst)
+    run_cmd(cmd)
+
+
+def run_cmd(cmd):
+    print("running:\t{0}".format(cmd))
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    process.wait()
+
+
+tests_path = "./tests"
+stockpile_path = "./stockpile"
+for test_type in sorted(os.listdir(tests_path)):
+    test_type_path = "{0}/{1}".format(tests_path, test_type)
+    test_path = "{0}/{1}".format(test_type_path, "*")
+
+    cp(test_path, stockpile_path)
+    data(test_type)
+
+    src = "./data.png"
+    dst = "./graphs/{0}.png".format(test_type)
+    mv(src, dst)
+    reset()
---
+++ ndsu_cyber/new/testing/progs/data.py
@@ -0,0 +1,78 @@
+# Collin Gros
+# 12/21/18
+#
+# Draw graph for each tests' train_stats.txt file
+#
+import os
+import matplotlib.pyplot as plt
+import argparse
+
+
+def read_info(item, var_type):
+    with open(item, "r") as info:
+        for line in info:
+            line_substr = line.split(":")
+            if len(line_substr) < 2:
+                continue
+
+            cur_type = line_substr[0]
+            cur_val = line_substr[1]
+
+            if cur_type == var_type:
+                return cur_val
+
+    return -1
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i")
+
+args = parser.parse_args()
+if not args.i:
+    print("no type specified, exiting...")
+    exit()
+
+var_type = args.i
+fig = plt.figure()
+plt.rc("xtick", labelsize=6)
+plt.rc("ytick", labelsize=6)
+x = []
+y = []
+# graph x axis' name is var_type (set up here)
+# graph y axis' name is range 0 to (num of images)
+
+tests = "./stockpile"
+for test in sorted(os.listdir(tests)):
+    test_path = tests + "/" + test
+    info_path = test_path + "/" + "train_info.txt"
+
+    try:
+        cur_val = int(read_info(info_path, var_type))
+    except:
+        cur_val = str(read_info(info_path, var_type))
+
+    stats_path = test_path + "/" + "train_stats.txt"
+    cur_viewed = int(read_info(stats_path, "viewed"))
+    cur_total = int(read_info(stats_path, "total"))
+
+    cur_diff = abs(cur_viewed - cur_total)
+
+    x.append(cur_val)
+    y.append(cur_diff)
+
+x = sorted(x)
+y = sorted(y)
+plt.plot(x, y)
+
+print(x)
+print(y)
+print("\n")
+
+goal = y
+for i in range (0, len(goal)):
+    goal[i] = 9
+print(x)
+print(goal)
+plt.plot(x, goal)
+
+fig.savefig("./data.png")
